aa68f00f21c56664535cfd3d9118442893fcb25f
Alex <alexzhangs@gmail.com>
2017-12-20 11:59:53 +0800

    Add comments

--- xsh.sh
+++ xsh.sh
@@ -14,6 +14,7 @@ function xsh () {
         return 255
     fi
 
+    # check input
     if [[ -n $1 ]]; then
         command=$(echo "$1" | tr 'A-Z' 'a-z')
         shift
@@ -21,11 +22,20 @@ function xsh () {
         return 255
     fi
 
+    #@private
+    # Source a function by relative file path and
+    # file name(without extension).
+    # Link a script by relative file path and
+    # file name(without extension).
     function __xsh_load () {
+        # handle functions
         if [[ -f ${XSH_HOME}/functions/${1}.sh ]]; then
+            # source "function <foo>" within "functions/<domain>/<foo>.sh"
+            # as "function x-<domain>-<foo>"
             source /dev/stdin \
                    <<<"$(sed "s|^function ${1##*/} ()|function x-${1/\//-} ()|" \
                              "${XSH_HOME}/functions/${1}.sh")"
+        # handle scripts
         elif [[ -f ${XSH_HOME}/scripts/${1}.sh ]]; then
             # link "scripts/<domain>/<foo>.sh"
             # as "/usr/local/bin/x-<domain>-<foo>"
@@ -35,9 +45,13 @@ function xsh () {
         fi
     }
 
+    # @private
+    # Call a function or a script by relative file path
+    # and file name(without extension).
     function __xsh_call () {
         local command
 
+        # check input
         if [[ -n $1 ]]; then
             command=$1
             shift
@@ -52,6 +66,7 @@ function xsh () {
         fi
     }
 
+    # main
     case $command in
         load)
             for name in "$@"; do
@@ -71,7 +86,9 @@ function xsh () {
             ;;
     esac
 
+    # clean
     unset __xsh_load __xsh_call
+
     return $ret
 }
 export -f xsh
