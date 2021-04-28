32dbc729d8c7272d5220d732ca7307b51a68c9b0
Alex <alexzhangs@gmail.com>
2021-04-29 01:12:21 +0800

    Update foo_spec.sh

--- spec/foo_spec.sh
+++ spec/foo_spec.sh
@@ -15,7 +15,7 @@ Describe 'Foo'
     When call xsh log 'LOG'
     The status should be success
     The output should include 'LOG'
-    #The variable XSH_HOME should be exported
-    #The result of function exported_functions should include 'xsh'
+    The variable XSH_HOME should be exported
+    The result of function exported_functions should include 'xsh'
   End
 End
