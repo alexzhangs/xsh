15cc10d10a589268facc94db8984b762294eee41
Alex <alexzhangs@gmail.com>
2021-04-29 02:00:51 +0800

    Update xsh.sh

--- xsh.sh
+++ xsh.sh
@@ -10,30 +10,6 @@ function xsh () {
         trap "${command}" RETURN
     }
 
-    function __xsh_log () {
-        declare level
-        level=$(echo "$1" | tr "[:lower:]" "[:upper:]")
-
-        declare caller
-        if [[ ${FUNCNAME[1]} == xsh && ${#FUNCNAME[@]} -gt 2 ]]; then
-            caller=${FUNCNAME[2]}
-        else
-            caller=${FUNCNAME[1]}
-        fi
-
-        case ${level} in
-            WARNING|ERROR|FAIL|FATAL)
-                printf "${caller}: ${level}: %s\n" "${*:2}" >&2
-                ;;
-            DEBUG|INFO)
-                printf "${caller}: ${level}: %s\n" "${*:2}"
-                ;;
-            *)
-                printf "${caller}: %s\n" "$*"
-                ;;
-        esac
-    }
-
     function __xsh_get_internal_functions () {
         declare -f xsh \
             | awk '$1 == "function" && match($2, "^__xsh_") > 0 && $3 == "()" {print $2}'
@@ -41,11 +17,9 @@ function xsh () {
 
     function __xsh_clean () {
         unset -f $(__xsh_get_internal_functions)
-        unset XSH_DEBUG
-        unset XSH_DEV
     }
 
-    __xsh_"${1}" "${@:2}"
-    __xsh_clean
+    __xsh_trap_return '
+        __xsh_clean;'
 }
 export -f xsh
