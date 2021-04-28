7464a47abf9f2a7136b29e841038340494e74505
Alex <alexzhangs@gmail.com>
2021-04-29 03:04:03 +0800

    Update xsh.sh

--- xsh.sh
+++ xsh.sh
@@ -3,7 +3,6 @@ function xsh () {
     function __xsh_clean () {
         unset -f $(declare -f xsh | awk '$1 == "function" && match($2, "^__xsh_") > 0 && $3 == "()" {print $2}')
         unset XSH_DEBUG
-        unset XSH_DEV
     }
 
     declare command="
