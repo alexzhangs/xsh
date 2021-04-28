a2c2fdb4c541dec6fba02c68a76b6b04501447c9
Alex <alexzhangs@gmail.com>
2021-04-29 03:36:29 +0800

    Update xsh.sh

--- xsh.sh
+++ xsh.sh
@@ -1,9 +1,5 @@
 function xsh () {
-
-    function __xsh_clean () {
-        unset -f $(echo __xsh_clean); :
-    }
-
-    __xsh_clean
+    function __xsh_clean () { unset -f $(echo __xsh_clean); :; }
+    trap "trap - RETURN; __xsh_clean;" RETURN
 }
 export -f xsh
