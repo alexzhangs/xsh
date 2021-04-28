c5cbe966733b981c4a8521443c480eb5db41551e
Alex <alexzhangs@gmail.com>
2021-04-29 02:32:13 +0800

    Update xsh.sh

--- xsh.sh
+++ xsh.sh
@@ -1,12 +1,7 @@
 function xsh () {
 
-    function __xsh_get_internal_functions () {
-        declare -f xsh \
-            | awk '$1 == "function" && match($2, "^__xsh_") > 0 && $3 == "()" {print $2}'
-    }
-
     function __xsh_clean () {
-        unset -f $(__xsh_get_internal_functions)
+        unset -f __xsh_clean
         unset XSH_DEBUG
         unset XSH_DEV
     }
