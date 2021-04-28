a1a6b8c05f5036429cdd6bacb4b831b0c384bcfb
Alex <alexzhangs@gmail.com>
2021-04-29 01:02:33 +0800

    Update foo_spec.sh

--- spec/foo_spec.sh
+++ spec/foo_spec.sh
@@ -3,12 +3,12 @@ Describe 'Foo'
   exported_functions() { declare -Fx | awk '{print $3}'; }
 
   It 'call function: foo'
-    foo() { trap 'if [[ ${FUNCNAME} == foo ]]; then trap - RETURN; fi' RETURN; echo FOO; export FOO=1; function bar () { echo BAR; }; export -f bar; }
+    foo() { trap 'if [[ ${FUNCNAME} == foo ]]; then trap - RETURN; fi' RETURN; echo FOO; export FOO=1; export -f foo; }
     When call foo
     The status should be success
     The output should equal 'FOO'
     The variable FOO should be exported
-    The result of function exported_functions should include 'bar'
+    The result of function exported_functions should include 'foo'
   End
 
   It 'call function: xsh log'
