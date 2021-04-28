42ae6ddda75922a0422d7293f6875162b9b6f1a2
Alex <alexzhangs@gmail.com>
2021-04-29 00:05:45 +0800

    Update foo_spec.sh

--- spec/foo_spec.sh
+++ spec/foo_spec.sh
@@ -1,9 +1,12 @@
 Describe 'Foo'
+  exported_functions() { declare -Fx | awk '{print $3}'; }
+
   It 'call function foo'
-    foo() { echo FOO; export FOO=1; }
+    foo() { echo FOO; export FOO=1; function bar () { echo BAR; }; export -f bar; }
     When call foo
     The status should be success
     The output should equal 'FOO'
     The variable FOO should be exported
+    The result of function exported_functions should include 'bar'
   End
 End
