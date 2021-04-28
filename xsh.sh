3951be36e02ace5a301c68102ec3a1af349208bd
Alex <alexzhangs@gmail.com>
2021-04-29 02:53:00 +0800

    Update xsh.sh

--- xsh.sh
+++ xsh.sh
@@ -1,19 +1,11 @@
 function xsh () {
 
-    function __xsh_get_internal_functions () {
-        declare -f xsh | awk '$1 == "function" && match($2, "^__xsh_") > 0 && $3 == "()" {print $2}'
-    }
-
-    function __xsh_clean () {
-        unset -f $(declare -f xsh | awk '$1 == "function" && match($2, "^__xsh_") > 0 && $3 == "()" {print $2}')
-        unset XSH_DEBUG
-        unset XSH_DEV
-    }
-
     declare command="
     if [[ \${FUNCNAME} == xsh ]]; then
         trap - RETURN
-        __xsh_clean
+        unset -f $(declare -f xsh | awk '$1 == "function" && match($2, "^__xsh_") > 0 && $3 == "()" {print $2}')
+        unset XSH_DEBUG
+        unset XSH_DEV
     fi;"
 
     trap "${command}" RETURN
