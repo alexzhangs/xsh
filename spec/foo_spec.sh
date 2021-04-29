329477db6e10061ef339dd5292ecf712bd37d464
Alex <alexzhangs@gmail.com>
2021-04-29 22:01:54 +0800

    Update foo_spec.sh

--- spec/foo_spec.sh
+++ spec/foo_spec.sh
@@ -1,6 +1,11 @@
 Describe 'Foo'
-  Include xsh.sh
-  exported_functions() { declare -Fx | awk '{print $3}'; }
+  xsh () {
+    function __xsh_clean () { unset -f $(echo __xsh_clean); :; }
+    trap "trap - RETURN; __xsh_clean;" RETURN
+    export XSH_HOME=~/.xsh
+  }
+  export -f xsh
+  exported_functions () { declare -Fx | awk '{print $3}'; }
 
   It 'call function: xsh'
     When call xsh
