185470b7f8e4756f3080b0c12bd962dc53d47a39
Alex <alexzhangs@gmail.com>
2021-05-12 17:30:48 +0800

    blackbox test

--- xsh.sh
+++ xsh.sh
@@ -2322,9 +2322,9 @@ function xsh () {
     #?
     function __xsh_clean () {
         # shellcheck disable=SC2046
-        unset -f $(__xsh_get_internal_functions)
         unset XSH_DEBUG
         unset XSH_DEV
+        unset -f $(__xsh_get_internal_functions)
     }
 
 
